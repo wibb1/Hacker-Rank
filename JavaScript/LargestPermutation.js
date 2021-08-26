9d59637387a821eb970952a8d7ed8dfa0d357707
Will Campbell <wibb1@comcast.net>
2021-08-26 10:24:37 -0400

    Largest Permutation

--- JavaScript/LargestPermutation.js
+++ JavaScript/LargestPermutation.js
@@ -1,41 +1,33 @@
-// function largestPermutation(k, arr) {
-//   let max = Math.max(...arr);
-//   for (let i = 0; i < k; i++) {
-//     for(let j=0;j<arr.length;j++){
-//       if (arr[j] === max-i){
-//         [arr[i], arr[j]] = [arr[j], arr[i]]
-//       }
-//     }
-//   }
-//   return arr;
-// }
+/*******
+ * Inputs:
+ *    k = number of swaps
+ *    arr = array of values
+ *
+ * Limits:
+ *    1 <= i <= unknown
+ *    1 <= arr[i] <= 10**5
+ *    1 <= k <= 10**9
+ *
+ * Tasks:
+ *    determine the index of the highest k values
+ *    determine the order that the values should be in
+ *    swap the values according to the order
+ */
 
 function largestPermutation(k, arr) {
+  if (k >= arr.length - 1) return arr.sort((a, b) => b - a);
   let sorted = [...arr].sort((a, b) => b - a);
   let idx;
-  const swap = (a,b) => {
-      [arr[a], arr[b]] = [arr[b], arr[a]];
+  for (let i = 0; i < arr.length && k > 0; i++) {
+    if (arr[i] !== sorted[i]) {
+      idx = arr.indexOf(sorted[i]);
+      [arr[i], arr[idx]] = [arr[idx], arr[i]];
       k--;
-  }
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== sorted[i] && k > 0) {
-      swap(i, arr.indexOf(sorted[i]))
     }
   }
   return arr;
 }
 
-// function largestPermutation(k, arr) {
-//   const sorted = [...arr].sort((a, b) => b - a);
-//   const swap = (a, b) => ([arr[a], arr[b]] = [arr[b], arr[a]]) && k--;
-
-//   for (var i = 0; i < arr.length && k > 0; i++) {
-//     if (arr[i] === sorted[i]) continue;
-//     swap(i, arr.indexOf(sorted[i], i));
-//   }
-
-//   return arr;
-// }
-
-console.log(largestPermutation(1, [4, 2, 3, 5, 1])); // [ 5,4,3,2,1]
-console.log(largestPermutation(2, [2, 1]));
+console.log(largestPermutation(3, [4, 2, 3, 5, 1])); // [ 5,4,3,2,1]
+console.log(largestPermutation(2, [2, 1])); //[2,1]
+console.log(largestPermutation(6, [2, 1])); //[2,1]
