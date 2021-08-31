b329223ce9575f77d5d90684bf5a6268f8e312ad
Will Campbell <wibb1@comcast.net>
2021-08-30 20:28:13 -0400

    Hash Tables: Ice Cream Parlor

--- JavaScript/HashTables-IceCreamParlor.js
+++ JavaScript/HashTables-IceCreamParlor.js
@@ -11,7 +11,13 @@ Constraints:
 n<=n<=5*10**4
 1<=cost[i]<=10**9
 
-steps - make an object with the name of the number and the value of the index in an array
+steps 
+1 - make an object with the name of the number and the value of the index in an array.  If there is more than one value push it into the array
+
+2 - if the value less money is in the object and it is not the same as the value then return the value of the first value of the array
+
+3 - if value less money is equal to to the value but there are more than one of the indexes in the array then return the second value
+
 
 */
 function whatFlavors(cost, money) {
