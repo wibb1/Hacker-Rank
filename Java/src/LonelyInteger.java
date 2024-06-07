a0f4bec36e4be535632b4957b084f6843314ff45
Will Campbell <62625260+wibb1@users.noreply.github.com>
2024-06-07 11:56:44 -0400

    lonely Integer

--- Java/src/LonelyInteger.java
+++ Java/src/LonelyInteger.java
@@ -1,58 +1,35 @@
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
 
 class Result {
-
-    /*
-     * Complete the 'lonelyinteger' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts INTEGER_ARRAY a as parameter.
-     */
-
-    public static int lonelyinteger(List<Integer> a) {
-        List<Integer> occurred = new ArrayList<Integer>();
-        List<Integer> occurredOnce = new ArrayList<Integer>();
+    public static int lonelyInteger(List<Integer> a) {
+        Map<Integer, Integer> occurrances = new HashMap<>();
         for (Integer num : a) {
-            if (!occurred.contains(num)) {
-                occurred.add(num);
-                occurredOnce.add(num);
+            if (occurrances.containsKey(num)) {
+                occurrances.replace(num, occurrances.get(num) + 1);
             } else {
-                occurredOnce.remove(num);
+                occurrances.put(num, 1);
             }
         }
-        return occurredOnce.get(0);
+        return getSingleValue(occurrances);
 
+    }
+
+    private static int getSingleValue(Map<Integer, Integer> entries) {
+        for (Map.Entry<Integer, Integer> entry : entries.entrySet()) {
+            if (Objects.equals(1, entry.getValue())) {
+                return entry.getKey();
+            }
+        }
+        return -1;
     }
 
 }
 
-public class Solution {
+public class LonelyInteger {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        int n = Integer.parseInt(bufferedReader.readLine().trim());
-
-        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-            .map(Integer::parseInt)
-            .collect(toList());
-
-        int result = Result.lonelyinteger(a);
-
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedReader.close();
-        bufferedWriter.close();
+        System.out.println(Result.lonelyInteger(new ArrayList<>(Arrays.asList(1))));
+        System.out.println( Result.lonelyInteger(new ArrayList<>(Arrays.asList(1, 1, 2))));
+        System.out.println( Result.lonelyInteger(new ArrayList<>(Arrays.asList(0,0, 1, 2, 1))));
     }
 }
